bbe04e87ec3763bd078159b967e4c582b6f3e156
shlomit <shlomitshifraw@gmail.com>
2023-06-09 16:55:36 +0300

    bug: the value of the buyer in the transacation object was 0because in the server, in the transacation schema I wrote that the type of the buyer is currency by mistake intead of string. feat: create the third row of the dasboard ui and connecting it to the server.

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -7,9 +7,8 @@ loadType(mongoose);
 const TransactionSchema = new Schema(
   {
     buyer: {
-      type: mongoose.Types.Currency,
-      currency: "USD",
-      get: (v) => v / 100,
+      type: String,
+      requierd: true
     },
     amount: {
       type: mongoose.Types.Currency,
